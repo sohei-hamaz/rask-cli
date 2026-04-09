a4e24f2d390eeaa9ed22b7030649df5d091107da
sohei-hamaz <p1wc41xe@s.okayama-u.ac.jp>
2026-04-09 16:11:01 +0900

    create tasks.rs

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,15 @@
 use anyhow::{Context, Result}; // Error handling library for better error messages
 use clap::Parser; // CLI argument parser
 use reqwest::blocking::Client; // HTTP client for making requests
-use serde::{Serialize, Deserialize}; // For serializing data structures to JSON
 use dotenvy::dotenv;
 use std::env; // For accessing environment variables
+mod tasks;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
     /// The pattern to look for
     pattern: Vec<String>,
-}
-
-#[derive(Serialize, Debug)]
-struct TaskRequest {
-    assigner_id:   i32,
-    content:       String,
-    due_at:        Option<String>,
-    description:   Option<String>,
-    project_id:    Option<i32>,
-    task_state_id: i32,
-    tags: Option<Vec<String>>,
-}
-
-#[derive(Deserialize, Debug)]
-struct UserResponse {
-    id:      i32,
-    name:    String,
-}
-
-#[derive(Deserialize, Debug)]
-struct ProjectResponse {
-    id:      i32,
-    name:    String,
-}
-
-#[derive(Deserialize, Debug)]
-struct TagsResponse {
-    id:      String,
-    name:    String,
-}
-
-#[derive(Deserialize, Debug)]
-struct TaskResponse {
-    id:          i32,
-    content:     String,
-    description: Option<String>,
-    due_at:      Option<String>,
-    created_at:  String,
-    updated_at:  String,
-    creator:     UserResponse,
-    assigner:    UserResponse,
-    project:     Option<ProjectResponse>,
-    tags:        Option<Vec<TagsResponse>>,
-    url:         String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -68,18 +24,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let pattern = args.pattern.join(" ");
     
     if pattern == "task list" {
-        let response = client
-            .get("http://localhost:3000/tasks.json") // 全員のタスクを表示するにはクエリを追加する必要あり
-            .query(&[("api_token", rask_api_token)]) 
-            .send()
-            .with_context(|| format!("could not fetch URL `{}`", "http://localhost:3000/tasks.json"))?;
+        tasks::list_tasks(&client, &rask_api_token)?;
+        // let response = client
+        //     .get("http://localhost:3000/tasks.json") // 全員のタスクを表示するにはクエリを追加する必要あり
+        //     .query(&[("api_token", rask_api_token)]) 
+        //     .send()
+        //     .with_context(|| format!("could not fetch URL `{}`", "http://localhost:3000/tasks.json"))?;
 
-        // println!("Status code: {}", response.status());
+        // // println!("Status code: {}", response.status());
 
-        let body = response
-            .text()
-            .with_context(|| "could not read response body")?;
-        println!("{}", body);
+        // let body = response
+        //     .text()
+        //     .with_context(|| "could not read response body")?;
+        // println!("{}", body);
     }
 
     Ok(())
